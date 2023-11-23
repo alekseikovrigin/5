e10c4a7db7394a18918cadaf3a71b3a06a958b6c
Aleksei Kovrigin <kovriginav@mail.ru>
2023-11-23 13:15:29 +0300

    refactor: splitting into multiple functions

--- structocaster.go
+++ structocaster.go
@@ -7,17 +7,14 @@ import (
 
 func castV(src interface{}, dest reflect.Value) {
 	tagName := "out"
-	if dest.Kind() == reflect.Interface && !dest.IsNil() {
-		elm := dest.Elem()
-		if elm.Kind() == reflect.Ptr && !elm.IsNil() && elm.Elem().Kind() == reflect.Ptr {
-			dest = elm
-		}
-	}
-	if dest.Kind() == reflect.Ptr && !dest.IsNil() {
-		dest = dest.Elem()
+
+	dest = dereferenceValue(dest)
+	if dest.Kind() != reflect.Struct || !dest.IsValid() {
+		return
 	}
 
-	if dest.Kind() != reflect.Struct || !dest.IsValid() {
+	SrcV := dereferenceValue(reflect.ValueOf(src))
+	if !SrcV.IsValid() {
 		return
 	}
 
@@ -34,44 +31,49 @@ func castV(src interface{}, dest reflect.Value) {
 			continue
 		}
 
-		SrcV := reflect.ValueOf(src)
-		if SrcV.Kind() != reflect.Ptr || SrcV.IsNil() {
-			continue
-		}
-		SrcV = SrcV.Elem()
-		if !SrcV.IsValid() {
-			continue
-		}
-
-		var FieldName string
-		var Value reflect.Value
-
-		if len(typeField.Tag.Get(tagName)) > 0 {
-			FieldName = typeField.Tag.Get(tagName)
-
-			if strings.Contains(FieldName, ".") {
-				Value = SrcV
-				words := strings.Split(FieldName, ".")
-				for _, word := range words {
-					if !Value.IsValid() {
-						break
-					}
-					Value = Value.FieldByName(word)
-				}
-			} else {
-				Value = SrcV.FieldByName(FieldName)
-			}
-		} else {
-			FieldName = typeField.Name
-			Value = SrcV.FieldByName(FieldName)
-		}
-
+		Value := getValueFromSrc(SrcV, typeField, tagName)
 		if !Value.IsValid() || !Value.Type().AssignableTo(valueField.Type()) {
 			continue
 		}
 
 		valueField.Set(Value)
 	}
+}
+
+func dereferenceValue(val reflect.Value) reflect.Value {
+	for val.Kind() == reflect.Ptr || val.Kind() == reflect.Interface {
+		if val.IsNil() {
+			return reflect.Value{}
+		}
+		val = val.Elem()
+	}
+	return val
+}
+
+func getValueFromSrc(src reflect.Value, field reflect.StructField, tagName string) reflect.Value {
+	var fieldName string
+	var value reflect.Value
+
+	if len(field.Tag.Get(tagName)) > 0 {
+		fieldName = field.Tag.Get(tagName)
+		if strings.Contains(fieldName, ".") {
+			value = src
+			words := strings.Split(fieldName, ".")
+			for _, word := range words {
+				if !value.IsValid() {
+					break
+				}
+				value = value.FieldByName(word)
+			}
+		} else {
+			value = src.FieldByName(fieldName)
+		}
+	} else {
+		fieldName = field.Name
+		value = src.FieldByName(fieldName)
+	}
+
+	return value
 }
 
 func Cast(SrcV interface{}, v interface{}) {
